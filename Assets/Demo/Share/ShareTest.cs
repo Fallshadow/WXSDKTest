79de6adf98c2399cd6582cc90f0520f9f2fb3999
Fallshadow <www.sunshuchao@outlook.com>
2024-09-05 22:15:09 +0800

    ADD 分享

---
+++ Assets/Demo/Share/ShareTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using WeChatWASM;
+
+public class ShareTest : MonoBehaviour {
+    public Button ShareBtn;
+
+    private void Start() {
+        ShareBtn.onClick.AddListener(Share);
+    }
+    public void Share() {
+        ShareAppMessageOption shareAppMessageOption = new ShareAppMessageOption();
+        shareAppMessageOption.title = "中秋博饼真有趣！";
+        shareAppMessageOption.imageUrl = "/images/background.jpg";
+        WX.ShareAppMessage(shareAppMessageOption);
+    }
+}
+
